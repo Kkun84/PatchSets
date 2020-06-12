383d2f0dd639ae42638bb6a40e91be7a35941737
Kkun84 <kesuike84@gmail.com>
2020-06-12 10:57:53 +0900

    Change varivable name

--- src/patch.py
+++ src/patch.py
@@ -5,36 +5,36 @@ import torch
 logger = getLogger(__name__)
 
 
-def make_patch1d(input, patch_size, patch_n, padding):
-    # intput axis: [batch, channels, features]
-    # output axis: [batch, sets, channels, features]
-    logger.debug(f"{input.shape=}, {patch_size=}, {patch_n=}, {padding=}")
-    if input.shape[2] - patch_size + padding == 0:
-        return input[:, None].repeat(1, patch_n, 1, 1)
+def make_patch1d(src, patch_size, patch_n, padding):
+    # src axis: [batch, channels, features]
+    # dst axis: [batch, sets, channels, features]
+    logger.debug(f"{src.shape=}, {patch_size=}, {patch_n=}, {padding=}")
+    if src.shape[2] - patch_size + padding == 0:
+        return src[:, None].repeat(1, patch_n, 1, 1)
     patch_index = [
-        torch.arange(input.shape[0])[:, None, None, None],
-        torch.arange(input.shape[1])[None, None, :, None],
-        torch.randint(input.shape[2] - patch_size, [input.shape[0], patch_n, 1, 1]) + torch.arange(patch_size)[None, None, None, :],
+        torch.arange(src.shape[0])[:, None, None, None],
+        torch.arange(src.shape[1])[None, None, :, None],
+        torch.randint(src.shape[2] - patch_size, [src.shape[0], patch_n, 1, 1]) + torch.arange(patch_size)[None, None, None, :],
     ]
     logger.debug(f"patch_index.shape={[i.shape for i in patch_index]}")
-    output = input[patch_index]
-    logger.debug(f"{output.shape=}")
-    return output
+    dst = src[patch_index]
+    logger.debug(f"{dst.shape=}")
+    return dst
 
 
-def make_patch2d(input, patch_size, patch_n, padding):
-    # intput axis: [batch, channels, height, width]
-    # output axis: [batch, sets, channels, height, width]
-    logger.debug(f"{input.shape=}, {patch_size=}, {patch_n=}, {padding=}")
-    if input.shape[2] - patch_size + padding == 0:
-        return input[:, None].repeat(1, patch_n, 1, 1, 1)
+def make_patch2d(src, patch_size, patch_n, padding):
+    # src axis: [batch, channels, height, width]
+    # dst axis: [batch, sets, channels, height, width]
+    logger.debug(f"{src.shape=}, {patch_size=}, {patch_n=}, {padding=}")
+    if src.shape[2] - patch_size + padding == 0:
+        return src[:, None].repeat(1, patch_n, 1, 1, 1)
     patch_index = [
-        torch.arange(input.shape[0])[:, None, None, None, None],
-        torch.arange(input.shape[1])[None, None, :, None, None],
-        torch.randint(input.shape[2] - patch_size, [input.shape[0], patch_n, 1, 1, 1]) + torch.arange(patch_size)[None, None, None, :, None],
-        torch.randint(input.shape[3] - patch_size, [input.shape[0], patch_n, 1, 1, 1]) + torch.arange(patch_size)[None, None, None, None, :],
+        torch.arange(src.shape[0])[:, None, None, None, None],
+        torch.arange(src.shape[1])[None, None, :, None, None],
+        torch.randint(src.shape[2] - patch_size, [src.shape[0], patch_n, 1, 1, 1]) + torch.arange(patch_size)[None, None, None, :, None],
+        torch.randint(src.shape[3] - patch_size, [src.shape[0], patch_n, 1, 1, 1]) + torch.arange(patch_size)[None, None, None, None, :],
     ]
     logger.debug(f"patch_index.shape={[i.shape for i in patch_index]}")
-    output = input[patch_index]
-    logger.debug(f"{output.shape=}")
-    return output
+    dst = src[patch_index]
+    logger.debug(f"{dst.shape=}")
+    return dst
