bde25de258823db1ab61f49c14f8a520f6eff2af
Kkun84 <kesuike84@gmail.com>
2020-06-12 11:26:27 +0900

    Change implementation of strings in logging

--- src/patch.py
+++ src/patch.py
@@ -11,7 +11,7 @@ def padding(src, axis, padding, pad_value=0):
         for a in axis:
             dst = padding(dst, a, padding, pad_value=0)
         return dst
-    logger.debug(f"{src.shape=}, {axis=}, {padding=}, {pad_value=}")
+    logger.debug(f"src.shape={src.shape}, axis={axis}, padding={padding}, pad_value={pad_value}")
     pad_shape = [1] * src.ndim
     pad_shape[axis] = padding
     pad = torch.full(pad_shape, pad_value)
@@ -22,7 +22,7 @@ def padding(src, axis, padding, pad_value=0):
 def make_patch1d(src, patch_size, patch_n, padding):
     # src axis: [batch, channels, features]
     # dst axis: [batch, sets, channels, features]
-    logger.debug(f"{src.shape=}, {patch_size=}, {patch_n=}, {padding=}")
+    logger.debug(f"src.shape={src.shape}, patch_size={patch_size}, patch_n={patch_n}, padding={padding}")
     if src.shape[2] - patch_size + padding == 0:
         return src[:, None].repeat(1, patch_n, 1, 1)
     patch_index = [
@@ -32,14 +32,14 @@ def make_patch1d(src, patch_size, patch_n, padding):
     ]
     logger.debug(f"patch_index.shape={[i.shape for i in patch_index]}")
     dst = src[patch_index]
-    logger.debug(f"{dst.shape=}")
+    logger.debug(f"dst.shape={dst.shape}")
     return dst
 
 
 def make_patch2d(src, patch_size, patch_n, padding):
     # src axis: [batch, channels, height, width]
     # dst axis: [batch, sets, channels, height, width]
-    logger.debug(f"{src.shape=}, {patch_size=}, {patch_n=}, {padding=}")
+    logger.debug(f"src.shape={src.shape}, patch_size={patch_size}, patch_n={patch_n}, padding={padding}")
     if src.shape[2] - patch_size + padding == 0:
         return src[:, None].repeat(1, patch_n, 1, 1, 1)
     patch_index = [
@@ -50,5 +50,5 @@ def make_patch2d(src, patch_size, patch_n, padding):
     ]
     logger.debug(f"patch_index.shape={[i.shape for i in patch_index]}")
     dst = src[patch_index]
-    logger.debug(f"{dst.shape=}")
+    logger.debug(f"dst.shape={dst.shape}")
     return dst
